a2e79790048241a8087ded505a538a880fc3a58e
dinuosi <2893416367@qq.com>
2022-11-07 14:29:40 +0800

    修改员工信息,包括禁用/解禁,回显数据进行修改,动态获取请求参数

---
+++ src/main/java/com/itheima/reggie/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.itheima.reggie.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName MybatisPlusConfig
+ * @Author ATao
+ * @Date 2022/11/5 15:05
+ * @Description 配置mbp分页插件
+ * @Since version-1.0
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.itheima.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.Employee;
 import com.itheima.reggie.service.EmployeeService;
@@ -8,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
+import org.apache.commons.lang.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
@@ -31,12 +33,13 @@ public class EmployeeController {
     /**
      * 员工登录
      * request 存session
+     *
      * @param request
      * @param employee
      * @return
      */
     @PostMapping("login")
-    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
 
         //1.将页面提交的密码password进行MD5加密处理
         String password = employee.getPassword();
@@ -44,32 +47,32 @@ public class EmployeeController {
 
         //2.根据页面提交的用户名username查询数据库
         LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(Employee::getUsername,employee.getUsername());
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
         Employee emp = employeeService.getOne(queryWrapper);
 
         //3.如果没有查询到则返回登录失败结果
-        if (emp==null){
+        if (emp == null) {
             return R.error("登录失败");
         }
 
         //4.密码比对,如果不一致则返回登录失败结果
-        if (!emp.getPassword().equals(password)){
+        if (!emp.getPassword().equals(password)) {
             return R.error("登录失败");
         }
 
         //5.查看员工状态,如果为已禁用状态,则返回员工已禁用结果
-        if (emp.getStatus() == 0){
+        if (emp.getStatus() == 0) {
             return R.error("账号已禁用");
         }
 
         //6.登录成功,将员工id存入Session并返回登陆成功结果
-        request.getSession().setAttribute("employee",emp.getId());
+        request.getSession().setAttribute("employee", emp.getId());
         log.info("登录成功");
         return R.success(emp);
     }
 
     @PostMapping("/logout")
-    public R<String> logout (HttpServletRequest request){
+    public R<String> logout(HttpServletRequest request) {
         request.getSession().removeAttribute("employee");
         log.info("登出成功");
         return R.success("登出成功");
@@ -77,11 +80,12 @@ public class EmployeeController {
 
     /**
      * 新增员工
+     *
      * @param employee
      * @return
      */
     @PostMapping
-    public R<Employee> save(HttpServletRequest request,@RequestBody Employee employee){
+    public R<Employee> save(HttpServletRequest request, @RequestBody Employee employee) {
         // 设置初始密码123456,需要进行MD5加密处理
         employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
         employee.setCreateTime(LocalDateTime.now());
@@ -93,7 +97,66 @@ public class EmployeeController {
         employee.setUpdateUser(empId);
 
         employeeService.save(employee);
-        log.info("新增员工,员工信息:{}",employee.toString());
+        log.info("新增员工,员工信息:{}", employee.toString());
         return null;
     }
+
+    /**
+     * 分页查询员工
+     *
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        log.info("接收到请求: page = {},pageSize = {},name = {},", page, pageSize, name);
+
+        // 构造分页构造器
+        Page pageInfo = new Page(page, pageSize);
+
+        // 构造条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(StringUtils.isNotEmpty(name), Employee::getName, name);
+
+        //添加排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+
+        // 执行查询
+        employeeService.page(pageInfo, queryWrapper);
+
+        return R.success(pageInfo);
+    }
+
+
+    @GetMapping("/{id}")
+    public R<Employee> getByID(@PathVariable Long id) {
+        log.info("根据id查询员工信息");
+        Employee employee = employeeService.getById(id);
+        if (employee != null){
+            return R.success(employee);
+        }
+        return R.error("未查询到该员工信息");
+    }
+
+    /**
+     * 根据id修改员工信息
+     *
+     * @param employee
+     * @return
+     */
+    @PutMapping()
+    public R<String> update(HttpServletRequest request, @RequestBody Employee employee) {
+        log.info(employee.toString());
+        log.info("接收到请求: id = {},status = {}", employee.getId(), employee.getStatus());
+
+        employee.setUpdateUser((Long) request.getSession().getAttribute("employee"));
+        employee.setUpdateTime(LocalDateTime.now());
+
+        employeeService.updateById(employee);
+
+        return R.success("修改成功");
+    }
+
 }
